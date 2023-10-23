42627a798fd8fe81f5481a4b5fe004074d6a6700
Ronney Arias <ronneyarias@gmail.com>
2023-10-23 13:51:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+     let words = tutorial.split(" ")
+    return words.map(word => {
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(" ")
+  })
 }
